59e2b7b5429ae4c5a11fcd95de7bba1440349690
Roberto Tyley <roberto.tyley@gmail.com>
2023-05-17 14:04:37 +0100

    Move the weird APIGateway adapters to their own file

--- src/main/scala/com/theguardian/content/rules/Lambda.scala
+++ src/main/scala/com/theguardian/content/rules/Lambda.scala
@@ -7,6 +7,7 @@ import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
 import com.madgag.scala.collection.decorators.*
 import com.theguardian.content.rules.logging.Logging
 import Credentials.fetchKeyFromParameterStore
+import com.theguardian.aws.apigateway.{ApiGatewayRequest, ApiGatewayResponse}
 
 import java.net.URI
 import java.net.http.HttpClient.Redirect
@@ -22,50 +23,7 @@ import scala.concurrent.duration.*
 import scala.concurrent.{Await, Future}
 import scala.jdk.FutureConverters.*
 import scala.util.{Failure, Success}
-import scala.jdk.CollectionConverters._
-
-class ApiGatewayResponse(
-  @BeanProperty var statusCode: Int,
-  @BeanProperty var headers: java.util.Map[String, String],
-  @BeanProperty var body: String
-)
-
-object ApiGatewayResponse {
-  def apply(statusCode: Int, headers: Map[String, String], body: String): ApiGatewayResponse = {
-    new ApiGatewayResponse(statusCode, headers.asJava, body)
-  }
-}
-
-class ApiGatewayRequest {
-  @BeanProperty var httpMethod: String = _
-  @BeanProperty var path: String = _
-  @BeanProperty var queryStringParameters: java.util.Map[String, String] = _
-  @BeanProperty var headers: java.util.Map[String, String] = _
-  @BeanProperty var body: String = _
-  @BeanProperty var base64Encoded: Boolean = false
-  @BeanProperty var stageVariables: java.util.Map[String, String] = _
-  @BeanProperty var requestContext: ApiGatewayRequestContext = _
-
-  private def asScalaMap[K, V](m: java.util.Map[K, V]): Map[K, V] = Option(m).map(_.asScala.toMap).getOrElse(Map.empty)
-  def queryStringParamMap: Map[String, String] = asScalaMap(queryStringParameters)
-  def headerMap: Map[String, String] = asScalaMap(headers)
-
-  override def toString = s"ApiGatewayRequest(" +
-    s"httpMethod = $httpMethod, " +
-    s"path = $path, " +
-    s"queryStringParameters = $queryStringParamMap, " +
-    s"headers = $headerMap, " +
-    s"base64Encoded = $base64Encoded, " +
-    s"stageVariables = $stageVariables, " +
-    s"requestContext = $requestContext  " +
-    s"body = $body)"
-}
-
-class ApiGatewayRequestContext {
-  @BeanProperty var stage: String = _
-
-  override def toString = s"ApiGatewayRequestContext(stage = $stage)"
-}
+import scala.jdk.CollectionConverters.*
 
 object Lambda extends Logging {
 //
